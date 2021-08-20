e17468c16ade72c0956a5adb6f34f19f20e1b340
Altynai23 <salyb1987@gmail.com>
2021-08-20 16:47:03 -0400

    OfficeHours Day12

---
+++ officeHours/Practice_08_18_2021/accessModifier/AccessModifier.java
@@ -0,0 +1,31 @@
+package Practice_08_18_2021.accessModifier;
+
+public class AccessModifier extends Person{
+    public static void main(String[] args) {
+
+
+        Person p=new Person();
+        p.name="";
+        //  p.age=0; // private is not visible
+        // p.ID=0;
+       /* AccessModifier accessModifier=new AccessModifier();
+        accessModifier.DoB="0";
+        */
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
+
+
+
