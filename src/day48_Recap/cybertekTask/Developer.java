cf67e31abeb4273150531595eee16192bef272dd
Altynai23 <salyb1987@gmail.com>
2021-08-20 12:06:44 -0400

    day48

---
+++ src/day48_Recap/cybertekTask/Developer.java
@@ -0,0 +1,31 @@
+package day48_Recap.cybertekTask;
+
+import java.time.LocalDate;
+
+public class Developer extends Employee{
+
+    public Developer(String name, char gender, LocalDate dOB, int employeeId, String jobTitle, double salary) {
+        super(name, gender, dOB, employeeId, jobTitle, salary);
+    }
+
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is coding");
+    }
+
+    @Override
+    public void attendMeeting() {
+        System.out.println(getJobTitle()+" "+getName()+" is attending grooming meeting");
+    }
+
+    @Override
+    public void sleep() {
+        System.out.println(getName()+" sleeps 5 hours");
+    }
+
+
+
+
+
+
+}
