991f5a2617f861e3289740894b534ee18e9168eb
Altynai23 <salyb1987@gmail.com>
2021-08-27 12:09:12 -0400

    day50

--- src/Exceptions/RectangleObjects.java
+++ src/Exceptions/RectangleObjects.java
@@ -2,12 +2,28 @@ package Exceptions;
 
 public class RectangleObjects {
 
-    public static void main(String[] args) {
+    public void method() throws Throwable{
+
+        this.finalize(); // clean up
+    }
+
+    public final static void main(String[] args) {
 
         Rectangle r1 = new Rectangle(5, -4);
 
         System.out.println(r1);
 
+        String str = "Java";
+        str = "Python";
+
+        System.out.println(str);
+
 
     }
+
+    public void method1(){
+
+    }
+
+
 }
