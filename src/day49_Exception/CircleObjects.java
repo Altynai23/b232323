67a9554efffb871c16e17b42ce262f814fd610ad
Altynai23 <salyb1987@gmail.com>
2021-08-29 14:42:39 -0400

    day49

--- src/day49_Exception/CircleObjects.java
+++ src/day49_Exception/CircleObjects.java
@@ -1,5 +1,9 @@
 package day49_Exception;
 
+import sun.reflect.generics.tree.Tree;
+
+import java.util.Arrays;
+
 public class CircleObjects {
 
     public static void main(String[] args) {
@@ -7,6 +11,9 @@ public class CircleObjects {
         Circle circle1 = new Circle(10);
 
         System.out.println(circle1);
+
+
+
 
 
 
