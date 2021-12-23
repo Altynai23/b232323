d637d525afb342c18bc0438175cc23d807033aa1
Altynai23 <salyb1987@gmail.com>
2021-12-23 16:21:06 -0500

    Java Boot Camp

---
+++ src/boot_camp/ExceptionPractice.java
@@ -0,0 +1,21 @@
+package boot_camp;
+
+public class ExceptionPractice {
+
+        public static void main(String[] args) {
+
+            // FileInputStream file = new FileInputStream(""); // checked exception
+
+
+            int[] arr = {1, 2, 3};
+
+            //    System.out.println( arr[2000] ); // unchecked exception
+
+            //     new ExceptionsPractice().finalize();
+
+
+        }
+
+
+
+}
